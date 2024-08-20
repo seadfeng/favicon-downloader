368314e7faf43d9e12632df213d9a6f4cb01a22a
Sead Feng <seadfeng@gmail.com>
2024-08-20 22:45:03 +0800

    update target

--- src/components/frontend/shared/footer.tsx
+++ src/components/frontend/shared/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <footer className="container py-4 text-center text-sm text-muted-foreground mt-10">
       <div className="my-3 flex items-center justify-center">
         <div className="mr-3">Our Products: </div>
-        <a href="https://www.redirectchecker.org/" target="_blank" className="text-primary flex items-center">
+        <a href="https://www.redirectchecker.org/" className="text-primary flex items-center">
           <img alt="Redirect Checker" src="https://www.faviconextractor.com/favicon/www.redirectchecker.org" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
           <span>Redirect Checker</span>
         </a>
