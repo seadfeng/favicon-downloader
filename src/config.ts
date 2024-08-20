3ed578ed00f8023c724e4cec858ce34a84aa40c0
Sead Feng <seadfeng@gmail.com>
2024-08-20 14:02:04 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export type LocaleType = typeof locales[number];
 export const appConfig = {
   appDomain: process.env.NODE_ENV === 'development' ? "127.0.0.1:3000" : "www.faviconextractor.com",
   appRootDomain: "faviconextractor.com",
-  appName: "FaviconExtractor.com",
+  appName: "FaviconExtractor",
   appDescription: "FaviconExtractor",
   gaId: process.env.NEXT_PUBLIC_GA_ID,
   origin: process.env.NODE_ENV === "development" ? "http://127.0.0.1:3000" : "https://www.faviconextractor.com",
